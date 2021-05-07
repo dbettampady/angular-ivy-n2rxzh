1e89d404499b6eab51fabf2b16297549a1f732f6
dbettampady <divya.bettampadi@gmail.com>
2021-05-07 15:14:12 +0530

    2 files modified, 1 added

---
+++ src/app.models.ts
@@ -0,0 +1,8 @@
+export class Details {
+  constructor(
+    public itemName?: string,
+    public quantity?: number,
+    public itemPrice?: number,
+    public total?: number
+  ) {}
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component, VERSION } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { Details } from '../app.models';
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = 'typescriptproj';
+  public itemName?: string;
+  public quantity?: number=0;
+  public itemPrice?: number=0;
+  public total?: number;
+  
+  public listofDetails: Details[];
+  
+  constructor() {
+    this.listofDetails = new Array<Details>();
+  }
+  public addRow() {
+	let total = this.itemPrice * this.quantity;
+    let details = new Details(this.itemName, this.quantity, this.itemPrice, total);
+    this.listofDetails.push(details);
+  }
 }
